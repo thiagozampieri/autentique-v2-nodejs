bc491aa0264374c9ae1d43f1a6b1dbf0b57e842d
Thiago Zampieri <thiago.zampieri@gmail.com>
2020-08-06 01:37:40 -0300

    fix url autentique

--- src/common/Api.js
+++ src/common/Api.js
@@ -1,9 +1,9 @@
 'use strict'
 import axios from 'axios'
-import { AUTENTIQUE_URL, AUTENTIQUE_TOKEN } from 'babel-dotenv'
+import { AUTENTIQUE_TOKEN } from 'babel-dotenv'
 
 const Api = (token) => axios.create({
-  baseURL: AUTENTIQUE_URL,
+  baseURL: 'https://api.autentique.com.br/v2',
   timeout: 1000,
   headers: {
     Authorization: `Bearer ${token || AUTENTIQUE_TOKEN}`
